e7674a1c3ed18ee8b73569fe2b5dc0f24d7a75df
Sebastian Ramirez <110700982+sebastianpain@users.noreply.github.com>
2023-05-23 23:42:10 +0000

    3

--- src/productManager.js
+++ src/productManager.js
@@ -1,55 +1,38 @@
-const fs = require('fs')
+const fs = require("fs");
 
-class productManager{
-    constructor(patch){
-        this.patch = patch
-    }
-    async getAllProducts(){
-        let pr = await fs.promises.readFile(this.patch, 'utf-8')
-        let prParse = JSON.parse(pr)
-        if(prParse.length <= 0){
-            console.log('No hay productos en la base de datos')
-        }else{
-            console.log(prParse)
-        }
-        console.log(prParse)
-    }
-    async deleteProducto(id){
-        let pr = await fs.promises.readFile(this.patch, 'utf-8')
-        let prParse = JSON.parse(pr)
-        
-        
-        const arrayNew = prParse.filter((ele) => {
-            return ele.id !== id
-        })
-        
-        await fs.promises.writeFile(this.patch,JSON.stringify(arrayNew,null,2), 'utf-8')
-    console.log('Producto Eliminado')
-    }
-    async updateProduct(id,infoNew){
-        let pr = await fs.promises.readFile(this.patch, 'utf-8')
-        let prParse = JSON.parse(pr)
+class ProductManager {
+  constructor(patch) {
+    this.patch = patch;
+  }
 
-        let arrayUpdated = prParse.map((ele)=>{
-            if(ele.id == id){
-                return {...ele, title:infoNew.title,price:infoNew.price}
-            
-            } else{
-                return ele
-            }
-        })
-        console.log(arrayUpdated)
-        await fs.promises.writeFile(this.patch, JSON.stringify(arrayUpdated,null,2), 'utf-8')
-        
-        console.log('producto Actualizado!')   
-        
+  async getAllProducts() {
+    try {
+      const pr = await fs.promises.readFile(this.patch, "utf-8");
+      const prParse = JSON.parse(pr);
+      if (prParse.length <= 0) {
+        console.log("No hay productos en la base de datos");
+      } else {
+        console.log(prParse);
+      }
+    } catch (error) {
+      console.log("Error al leer el archivo:", error);
     }
-    
+  }
+
+  async getProductById(id) {
+    try {
+      const pr = await fs.promises.readFile(this.patch, "utf-8");
+      const prParse = JSON.parse(pr);
+      const product = prParse.find((ele) => ele.id === id);
+      if (product) {
+        console.log(product);
+      } else {
+        console.log("No se encontró ningún producto con el ID proporcionado");
+      }
+    } catch (error) {
+      console.log("Error al leer el archivo:", error);
+    }
+  }
 }
-let newPr = new productManager('./productos.json')
 
-//newPr.deleteProducto('dasdsadasdasddswwww2.2s2ssssdads')
-newPr.updateProduct('dasdsadasdasddswwww2.2s2ssssdads.321312',{price: 159999, title:"TITULO MODIFICADO"})
-newPr.getAllProducts()
-//fs.writeFileSync("./productos.json", "[]")
-module.exports = productManager;
+module.exports = ProductManager;
