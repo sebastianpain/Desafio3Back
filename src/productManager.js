717c9e10b70186c834b5d5ee3f60d8f41fa2f656
Sebastian Ramirez <110700982+sebastianpain@users.noreply.github.com>
2023-05-24 02:32:41 +0000

    3

--- src/productManager.js
+++ src/productManager.js
@@ -10,12 +10,12 @@ class ProductManager {
       const pr = await fs.promises.readFile(this.patch, "utf-8");
       const prParse = JSON.parse(pr);
       if (prParse.length <= 0) {
-        console.log("No hay productos en la base de datos");
+        return "No hay productos en la base de datos";
       } else {
-        console.log(prParse);
+        return prParse;
       }
     } catch (error) {
-      console.log("Error al leer el archivo:", error);
+      throw new Error("Error al leer el archivo: " + error);
     }
   }
 
@@ -25,12 +25,12 @@ class ProductManager {
       const prParse = JSON.parse(pr);
       const product = prParse.find((ele) => ele.id === id);
       if (product) {
-        console.log(product);
+        return product;
       } else {
-        console.log("No se encontró ningún producto con el ID proporcionado");
+        return "No se encontró ningún producto con el ID proporcionado";
       }
     } catch (error) {
-      console.log("Error al leer el archivo:", error);
+      throw new Error("Error al leer el archivo: " + error);
     }
   }
 }
